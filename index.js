8d474be5e0973f47034fd85c4fd99e8c358095e2
Tony Cetuk <cetukt@gmail.com>
2018-01-29 23:51:15 -0500

    Added time transformation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const GPS = require('gps');
 
 const file = 'COM3';
@@ -16,16 +17,21 @@ port.pipe(parser);
 let gps = new GPS();
 
 // Register the GPS data listener to log the data whenever data is received on a periodic basis.
-const LOG_EVERY_N_TIMES = 10;
+const LOG_EVERY_N_TIMES = 1; // Can change to every 5 seconds with a value of 5
 const gpsDataType = 'GGA'; // GGA, GSA, RMC, GSV
 let logCount = 0
 gps.on(gpsDataType, data => {
   if (logCount % LOG_EVERY_N_TIMES === 0) {
+		// const timestamp = moment(data.time).format('YYYY-MM-DD hh:mm:ss A');
+	  // console.log(`${timestamp}  ${data.lat}  ${data.lon}  ${data.alt}  ${data.satelites}  ${data.hdop}`);
+
+	  // console.log(`${data.time}  ${data.lat}  ${data.lon}  ${data.alt}  ${data.satelites}  ${data.hdop}`);
+		// console.log(data.time.toISOString());
     console.log(data);
   } else {
 	  console.log('Skipping...');
 	}
-	++logCount;
+++logCount;
 });
 
 // Register the GPS DMEA sentence parser to any data received from the serial port.
